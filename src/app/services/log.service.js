712b3bfbb72130ad9033d7eb46aa373616a8a014
AlisaWonder <korenskayaalice@gmail.com>
2021-12-17 17:26:43 +0300

    commit with files

---
+++ src/app/services/log.service.js
@@ -0,0 +1,25 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://311cfc665c22479da95ba5af7fbaac0c@o1062449.ingest.sentry.io/6052861",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+const logger = {
+  init,
+  log,
+};
+
+export default logger;
